279e6ec3cd44945147f1d22bcad682602cdc3e1a
Hugo Cornago <hello@zuperito.xyz>
2022-12-18 19:52:28 +0100

    añadir comentarios explicando cada funcion

--- src/bizi-main.cpp
+++ src/bizi-main.cpp
@@ -12,11 +12,20 @@
 #include <algorithm>
 using namespace std;
 
+/* Estructura de datos que almacenara información basica sobre
+ * un fichero (su nombre), y un puntero a la lectura del mismo con el fin de
+ * minimizar el numero de aperturas del fichero.
+ */
 struct Fichero {
     string nombre;
     ifstream* f;
 };
 
+/* Pre: ---
+ * Post: Imprime al usuario una lista de los posibles fichero que 
+ *       estan disponibles para el programa y pregunta cual quiere elegir.
+ *       La función devuelve una string con la ruta relativa correspondiente al fichero.
+ */
 string selectorDeFichero() {
     cout << "Elección de ficheros de usos y usuarios. Opciones disponibles: " << endl;
     cout << "16: octubre 2016 a marzo 2017" << endl;
@@ -28,9 +37,15 @@ string selectorDeFichero() {
     string opcion;
     cin >> opcion;
     string fichero = "datos/usos-" + opcion + ".csv";
+
     return fichero;
 }
 
+/* Pre: ---
+ * Post: Cambia el fichero <fichero> dependiendo de la salida
+ *       de la función selectorDeFichero().
+ *       La función modifica la componente <nombre> y <f> del parametro <fichero>.
+ */
 bool cambiarFichero(Fichero& fichero) {
     const auto& nombre_fichero = selectorDeFichero();
     ifstream fichero_abierto {nombre_fichero};
@@ -43,9 +58,14 @@ bool cambiarFichero(Fichero& fichero) {
     fichero.f = &fichero_abierto;
 
     cout << "El fichero \"" << fichero.nombre << "\" existe y ha sido seleccionado." << endl;
+    return true;
 }
 
-void pantallaUsos (const Fichero& fichero) {
+/* Pre: ---
+ * Post: Imprime en pantalla información sobre los usos del fichero
+ *       selecionado por el usuario.
+ */
+void pantallaUsos(const Fichero& fichero) {
     cout << "Orden: usos" << endl;
     cout << "Fichero de usos seleccionado actualmente: " << fichero.nombre << "." << endl;
     cout << "Número de usos como traslado: " << endl;
@@ -54,7 +74,12 @@ void pantallaUsos (const Fichero& fichero) {
     cout << "Orden: " << endl;
 }
 
-bool ordenUsos (Fichero& fichero) {
+/* Pre: <fichero> debe estar incializado y su componente <f> debe apuntar a un
+ *      fichero abierto con permisos de lectura.
+ * Post: Devuelve "true" si ha podido obtener el numero de usos de ...
+ *       En cualquier otro caso, devuelve "false".
+ */
+bool ordenUsos(Fichero& fichero) {
     string numTraslados;   
     string numCirculares;
     int numTotalUsos;
@@ -64,16 +89,20 @@ bool ordenUsos (Fichero& fichero) {
         getline(fichero_abierto, cabecera);
         string uso;
         while (getline(fichero_abierto, uso)){
-            numTotalUsos++;
-            if(getline(fichero_abierto, numTraslados))
+            // TODO
+            // numTotalUsos++;
+            // if(getline(fichero_abierto, numTraslados))
+            break;
         }
- 
-
      }
      
-
+    return true;
 }
 
+/* Pre: ---
+ * Post: Imprime en pantalla las posibles ordenes que estan
+ *       implementadas y puede usar el usuario.
+ */
 void imprimirOrdenesDisponibles () {
     cout << "ÓRDENES DISPONIBLES" << endl;
     cout << "===================" << endl;
@@ -93,33 +122,45 @@ void imprimirOrdenesDisponibles () {
     cout << "FIN: " << setw(9) << "Termina la ejecución de este programa." << endl;
 }
 
+/* Pre: ---
+ * Post: 
+ *       Devuelve "true" si el usuario a dado la orden "FIN".
+ *       Devuelve "false" en cualquier otro caso.
+ */
 bool ejecutarOrden(const string& orden, Fichero& fichero) {
     if (orden == "AYUDA") {
         imprimirOrdenesDisponibles();
-    } else
-    if (orden == "FICHERO") {
-        while (!cambiarFichero(fichero))
-    }
-    if (orden == "USOS"){
+    } else if (orden == "FICHERO") {
+        while (!cambiarFichero(fichero));
+    } else if (orden == "USOS") {
         ordenUsos(fichero);
-    }
-
-    else
-    if (orden == "FIN") {
+    } else if (orden == "FIN") {
         return true;
+    } else {
+        /* No existe la orden que el usuario a solicitado */
+        cerr << "No existe la orden \"" << orden << "\"." << endl;
     }
 
+    return false;
 }
 
+/* Pre: ---
+ * Post: Función intermedia que se encarga de pedir al usuario una orden,
+ *       almacenarla en una variable, convertir cada caracter en su mayuscula correspondiente
+ *       y llamar a la función ejectuarOrden con esa orden y el <fichero> proporcionado.
+ *       Devuelve la salida de la función ejectuarOrden.
+ */
 bool elejirOrden(Fichero& fichero) {
     string orden;
     cin >> orden;
 
-    /* Transformar cada caracter de la string <orden> a 
-       mayusculas con la orden std::toupper().
-    */
+    /* Con el fin de optimizar el programa, hemos optado por la funcion std::transform
+     * que ejecuta una instruccion en cada caracter de la string order.
+     */
     transform(orden.begin(), orden.end(), orden.begin(),
-        [](unsigned char c){ return std::toupper(c); });
+        /* Esta función lambda hace uso de std::toupper, aunque como tiene muchos overloads,
+         * dejamos que el compilador elija el mejor de ellos. */
+        [](unsigned char c){ return toupper(c); });
 
     return ejecutarOrden(orden, fichero);
 }
@@ -132,7 +173,8 @@ int main(){
     imprimirOrdenesDisponibles();
     while (!parar) {
         if (elejirOrden(f)) {
-            /* elejirOrden devuelve true cuando el usuario ha decidido salir del programa.*/
+            /* elejirOrden devuelve true cuando el usuario ha decidido salir del programa.
+             * En ese caso, saldremos del bucle. */
             parar = true;
         }
     }
