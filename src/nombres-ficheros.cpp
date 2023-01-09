ad764a8e46b464b84d1735b175efdffef2f6963e
Hugo Cornago <hello@zuperito.xyz>
2023-01-10 00:08:51 +0100

    cambiar implementacion del fichero "nombres-fichero"

--- src/nombres-ficheros.cpp
+++ src/nombres-ficheros.cpp
@@ -1,16 +1,18 @@
+/*********************************************************************************************\
+ * Programación 1. Trabajo obligatorio
+ * Autores: Hugo Cornago y Jaime Alonso
+ * Ultima revisión: 10-01-2023
+ * Resumen: Fichero de interfaz «nombres-ficheros.hpp» de un módulo que contiene constantes con
+ *          los nombres o partes de nombres de los ficheros que se van a utilizar y un
+ *          procedimiento denominado «construirNombreFicheroUsos» que facilita la construcción
+ *          del nombre de un fichero de usos a partir de la opción sobre el fichero de usos que
+ *          haya escrito el usuario. 
+\*********************************************************************************************/
+
+#include "nombres-ficheros.hpp"
 #include <string>
 #include <iostream>
-#include "nombres-ficheros.hpp"
 using namespace std;
-
-const string RUTA_DATOS = "datos/";
-const string PREFIJO_USOS = "usos-";
-const string EXTENSION = ".csv";
-const string FICHERO_ESTACIONES = RUTA_DATOS + "estaciones.csv";
-const string FICHERO_USUARIOS = RUTA_DATOS + "usuarios.csv";
-const string FICHERO_AYUDA = "res/ayuda.txt";
-const string FICHERO_OPCIONES = "res/opciones.txt";
-
 
 /*
  * Pre:  ---
@@ -22,22 +24,5 @@ const string FICHERO_OPCIONES = "res/opciones.txt";
  *       Si «opcion» tiene el valor "t2", devuelve la ruta "datos/usos-t2.csv".
  */
 string construirNombreFicheroUsos(const string opcion){
-    if (opcion == "16"| opcion == "17"| opcion == "t1"| opcion == "t2"){
-        return RUTA_DATOS + PREFIJO_USOS + opcion + EXTENSION;
-    }    
-    if (opcion == "estaciones"){
-        return FICHERO_ESTACIONES;
-    }
-    if (opcion == "usuarios"){
-        return FICHERO_USUARIOS;
-    }
-    if (opcion == "opciones"){
-        return FICHERO_OPCIONES;
-    }
-        if (opcion == "ayuda") {
-        return FICHERO_AYUDA;
-    }
-    else{
-        cerr << "Seleccione una opción válida" << endl;
-    }
+    return RUTA_DATOS + PREFIJO_USOS + opcion + EXTENSION;
 }
