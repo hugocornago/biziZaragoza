d6d457dbaccc3cdc57b90876c6a623ba0bdcb8ca
Hugo Cornago <hello@zuperito.xyz>
2023-01-10 13:28:55 +0100

    datos pruebas

--- src/0-datos-pruebas.cpp
+++ src/0-datos-pruebas.cpp
@@ -13,6 +13,19 @@
 using namespace std;
 
 
+/*
+ * Pre:  ---
+ * Post: Devuelve una ruta de acceso relativa a un fichero de usos partiendo del valor del 
+ *       parámetro «opcion», concatenando la ruta relativa de acceso al directorio de datos con
+ *       el prefijo común a los ficheros de usos, la opción y la extensión de los fichero de
+ *       usos.
+ *       Por ejemplo, si «opcion» tiene el valor "16", devuelve la ruta "datos/usos-16.csv".
+ *       Si «opcion» tiene el valor "t2", devuelve la ruta "datos/usos-t2.csv".
+ */
+string construirNombreFicheroUsos(const string opcion){
+    return RUTA_DATOS + PREFIJO_USOS + opcion + EXTENSION;
+}
+
 /* leer N lineas de un fichero */
 /* guardar en un vector de strings */
 /* string  lineas[N]*/
